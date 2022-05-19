40c4a4e525c59f7b07cb4668c29c0258d5c6e873
Syarguk <bulbaburak@gmail.com>
2022-05-19 22:48:59 +0300

    feat: 04-copy-directory completed

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const pathSourceDir = path.join(__dirname, 'files');
 
+fs.stat(path.join(__dirname, 'files-copy'), (err) => {
+    if (err) {/* throw err; */}
+    else { const pathDir = path.join(__dirname, 'files-copy');
+        fs.readdir(pathDir, (err, files) => {
+            if (err) throw err;
+            let arrFiles = [];
+            files.forEach(file => { arrFiles.push(file)});
+            arrFiles.forEach(file => {
+                fs.unlink(path.join(pathDir, file), err => {if (err) throw err});
+            })
+        })
+    }
+});
+fs.readdir(pathSourceDir, (err, files) => {
+    if (err) throw err;
+    fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+        if (err) throw err;
+        files.forEach(file => {
+            fs.copyFile(path.join(pathSourceDir, file), path.join(__dirname, 'files-copy', file), () => {
+            });
+        })
+    }
+)});
