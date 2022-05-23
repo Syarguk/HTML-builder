2d2ebd5df242b7b27ead55d5946e4f977e4306e1
Syarguk <bulbaburak@gmail.com>
2022-05-23 20:22:07 +0300

    fix: change file

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,23 +1,16 @@
 const fs = require('fs');
 const path = require('path');
-const pathDirProject = path.join(__dirname, 'project-dist');
-const pathDirStyles = path.join(__dirname, 'styles');
+const pathTarget = path.join(__dirname, 'project-dist', 'bundle.css');
+const pathSource = path.join(__dirname, 'styles');
 
-fs.writeFile(path.join(pathDirProject, 'bundle.css'), "", (err) => { if(err) throw err});
-fs.readdir(pathDirStyles, (err, files) => {
-    if (err) {console.log(err);}
-    else {files.forEach(file => {
+createBundle(pathSource, pathTarget);
+async function createBundle(source, target) {
+    await fs.promises.writeFile(target, "");
+    const files = await fs.promises.readdir(source);
+    for (let file of files) {
         if (file.includes("css")) {
-            fs.readFile(path.join(pathDirStyles, file), (err, data) => {
-                if (err) console.log(err);
-                else {
-                    fs.appendFile(
-                        path.join(pathDirProject, 'bundle.css'),
-                        "\n" + data.toString(),
-                        err => {if (err) throw err}
-                    );
-                }
-            });
+            let data = await fs.promises.readFile(path.join(source, file));
+            await fs.promises.appendFile(target, data.toString() + "\n");
         }
-    })};
-})
+    }
+}
