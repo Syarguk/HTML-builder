cc9ffeee159a5c53f850c3e5e5a037290de11835
Syarguk <bulbaburak@gmail.com>
2022-05-23 17:50:20 +0300

    fix: change file

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,18 +1,14 @@
 const fs = require('fs');
 const path = require('path');
-const pathFile = path.join(__dirname, 'secret-folder');
+const pathSecret = path.join(__dirname, 'secret-folder');
 const {stdout} = process;
 
-fs.readdir(pathFile,  {withFileTypes: true}, (err, files) => {
-    if (err) throw err;
-    for (let elem of files) {
-        let elName = path.parse(elem.name);
-        let pathEl = path.join(pathFile, elem.name);
-        if(elem.isFile()) {
-            fs.stat(pathEl, (err, stats) => {
-                if (err) throw err;
-                stdout.write(`${elName.name} - ${elName.ext.slice(1)} - ${stats.size / 1000}kb\n`);
-            });
+(async function readCat(pathDir) {
+    const files = await fs.promises.readdir(pathDir);
+    for (let file of files) {
+        const stats = await fs.promises.stat(path.join(pathDir, file));
+        if (stats.isFile()) {
+            stdout.write(`${file.slice(0, file.indexOf("."))} - ${file.slice(file.indexOf(".") + 1)} - ${stats.size / 1000}kb\n`);
         }
     }
-});
+})(pathSecret);
